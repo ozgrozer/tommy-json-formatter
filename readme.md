7ffad4a4dab22a8c88bb0c2c4624245ff4562773
Ozgur <ozgr@live.com>
2021-01-03 16:32:28 +0300

    First commit

---
+++ readme.md
@@ -0,0 +1,3 @@
+# tommy-json-formatter
+
+A JSON formatter app for [Tommy](https://github.com/ozgrozer/tommy)
---
+++ src/app.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import './app.scss'
+
+const App = () => {
+  return (
+    <div id='app'>
+      App
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
